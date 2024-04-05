0e65fb96d1650816f321252de4ee5ce2c88a79eb
mdmahfuzbipu <mdmahfuzbipu@gmail.com>
2024-04-05 12:18:20 +0600

    problem 39

--- DAY 11/problem_39.py
+++ DAY 11/problem_39.py
@@ -10,3 +10,13 @@ tpl = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 tpl1=tuple(i for i in tpl if i%2==0)
 print(tpl1)
+
+
+#solution 2
+
+tpl2=tuple(
+    filter(lambda x: x%2 == 0,tpl) #lambda func returns 1 if returns true
+        ) #filter function only keeps the true/1 values
+
+
+print(tpl2)
